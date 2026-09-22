6373efa9483f723aeee11ccfd0d5907da9c2315d
rishis26 <rishishah781@gmail.com>
2026-09-22 18:28:46 +0530

    added frontend service

---
+++ frontend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    `
+        <h1>Infrastructure Project</h1>
+        <p>Frontend is on port ${PORT} and running</p>
+        `,
+  );
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`frontend is working on http://localhost:${PORT}`);
+});
